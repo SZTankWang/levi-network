f7d15802c0069889de0928e20d22cf1d9072455a
zhenming wang <zhenmingwang@zhenmingdembp.ads.northwestern.edu>
2022-11-02 21:00:14 -0500

    sth

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path = require('path');
 const bodyParser = require("body-parser");
 
-const router = express.Router();
+// const router = express.Router();
 const app = express()
 const port = 3000
 
 app.use(express.static(__dirname));
 
+// add router in the Express app.
+// app.use("/", router);
 
 app.get('/', (req, res) => {
   // res.send('Hello World!')
@@ -21,11 +23,10 @@ app.listen(port, () => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.post("/submit",(request,response) => {
+app.post("/submit",(request,response) => {
 //code to perform particular action.
 //To access POST variable use req.body()methods.
 console.log(request.body);
+response.send({status:'received'});
 });
 
-// add router in the Express app.
-app.use("/", router);
