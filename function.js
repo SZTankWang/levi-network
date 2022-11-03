a3c4d2fd06ce6bc12ca4629ad1310ee8095570ea
zhenming wang <zhenmingwang@zhenmingdembp.ads.northwestern.edu>
2022-11-02 20:33:51 -0500

    sth

---
+++ function.js
@@ -0,0 +1,8 @@
+function submitForm(){
+    // console.log("hello world");
+    // window.alert("Login functionality is under development.");
+    const location = document.getElementById('location').value;
+    const comment = document.getElementById('comment').value;
+    // console.log(comment);
+
+}   
